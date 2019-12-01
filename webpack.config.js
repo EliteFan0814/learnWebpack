d7e028bec5635c0156bd58f771f7321d81f417ae
EliteFan <fan552426811@outlook.com>
2019-12-01 10:58:04 +0800

    url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -7,18 +7,30 @@ module.exports = {
     entry: {
         main: './src/index.js'
     },
-    module:{
-        rules:[
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
             {
-                test: /\.png$/,
-                use:{
-                    loader:'file-loader'
+                test: /\.(png|jpg|gif)$/,
+                use: {
+                    // loader: 'file-loader',
+                    // 也可以使用 url-loader
+                    loader: 'url-loader',
+                    // 打包配置选项
+                    options: {
+                        // 打包后的命名P
+                        name: '[name]_[hash].[ext]',
+                        outputPath: 'images',
+                        // url-loader limit 如果图片小超过2048个字节，就
+                        // 打包到 images 文件夹内，小于的话转为base64直接存储到目标js文件
+                        limit: 2048
+                    }
                 }
             }
         ]
     },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+
 }
