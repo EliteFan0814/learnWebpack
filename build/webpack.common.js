58a284da52cd1ff8961cd374de022aab6ce73d29
EliteFan <fan552426811@outlook.com>
2019-12-18 08:12:18 +0800

    split

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -68,7 +68,8 @@ module.exports = {
               useBuiltIns: 'usage',
               corejs: 3
             }]
-          ]
+          ],
+          plugins: ['@babel/plugin-syntax-dynamic-import']
         }
       }
     ]
@@ -83,4 +84,27 @@ module.exports = {
       to: 'font'
     }])
   ],
+  optimization: {
+    splitChunks: {
+      chunks: 'async',
+      minSize: 30000,
+      maxSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      automaticNameDelimiter: '~',
+      name: true,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true
+        }
+      }
+    }
+  }
 }
--- src/index2.js
+++ src/index2.js
@@ -1,6 +1,8 @@
 // import "core-js/stable";
 // import "regenerator-runtime/runtime";
+import _ from 'lodash'
 
+console.log(_.join(['a','b','c'],'###'))
 import {a} from './testMethods.js'
 let b = '哈哈哈哈'
 console.log('这是index2.js')
